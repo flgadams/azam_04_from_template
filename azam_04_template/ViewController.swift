f5189fdacdb3904e2fe4611f42b7f3a8c240b0cf
flgadams <flgadams@gmail.com>
2024-08-13 23:21:29 -0400

    Initial commit

---
+++ azam_04_template/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  azam_04_template
+//
+//  Created by Glenn Adams on 8/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.green
+    }
+
+
+}
+
